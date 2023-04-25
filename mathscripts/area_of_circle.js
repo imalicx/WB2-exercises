11531bac89dd31ec6cead14fd738b44665072398
imalicx <xenrecords@yahoo.com>
2023-04-25 13:52:34 -0400

    areaOfcircle

---
+++ mathscripts/area_of_circle.js
@@ -0,0 +1,11 @@
+// area
+
+var radiusOfCircle = 20;
+var areaOfCircle = radiusOfCircle ** 2;
+var pieOfCircle = 3.14;
+
+// calculation
+var totalDue = areaOfCircle * pieOfCircle;
+
+// output
+console.log("The area of a circle with radius " + radiusOfCircle + " is " + totalDue + ".");
---
+++ mathscripts/tip_amount.js
@@ -0,0 +1,12 @@
+// food amount
+
+var foodCost = 777.77;
+var tipPercent = 20;
+
+// calculations
+var tipAmount = (foodCost * tipPercent) / 100;
+var totalAmount = foodCost + tipAmount;
+
+// amount
+
+console.log("The tip on a $" + foodCost + " food bill is $" + tipAmount + ".");
