edfb9480eb0dcdced87751ebfdba9e65d03d8071
imalicx <xenrecords@yahoo.com>
2023-04-25 11:43:06 -0400

    areaOfrectangle

---
+++ mathscripts/area_of_rectangle.js
@@ -0,0 +1,12 @@
+// area of a rectangle
+
+var areaofRect = 15
+var lengthofRect = 30
+
+// Calculations
+
+var Totaldue = areaofRect * lengthofRect;
+
+// area
+
+console.log("The area of the rectangle is " +Totaldue);
