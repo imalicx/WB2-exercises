6b929e9bb50e121e16adddc2e7b92055a52db300
imalicx <xenrecords@yahoo.com>
2023-04-25 11:06:59 -0400

    networth

---
+++ mathscripts/net_worth.js
@@ -0,0 +1,13 @@
+// Calculating Networth
+
+var networth = 40000
+var debt =20000
+
+// calculations
+
+var Totaldue = networth - debt;
+
+// output
+
+console.log("Your net worth is $" +Totaldue);
+
