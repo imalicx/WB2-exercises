65d81f651bbd121e0a104f7809e57d89504959e6
imalicx <xenrecords@yahoo.com>
2023-04-27 09:19:41 -0400

    distance

---
+++ mathscripts/distance.js
@@ -0,0 +1,8 @@
+var x1 = 1;
+var y1 = 3;
+var x2 = 5;
+var y2 = 7;
+
+var distance = Math.hypot(x2 - x1, y2 - y1);
+
+console.log(`The distance between (${x1},${y1}) and (${x2},${y2}) is ${distance}`);
